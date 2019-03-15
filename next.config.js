6b34fc18d426294c3e2a83242914f107c9237fe0
Gilad Novik <gilad@novik.ca>
2019-03-15 05:26:14 +0200

    less support

--- next.config.js
+++ next.config.js
@@ -1,23 +1,25 @@
-module.exports = require('next-images')(
-    require('next-offline')({
-        webpack(config) {
-            config.node = {fs: 'empty'};
-            return config;
-        },
-        env: {},
-        workboxOpts: {
-            runtimeCaching: [
-                {
-                    urlPattern: /^https?.*/,
-                    handler: 'networkFirst',
-                    options: {
-                        cacheName: 'https-calls',
-                        networkTimeoutSeconds: 15,
-                        expiration: {maxEntries: 150, maxAgeSeconds: 30 * 24 * 60 * 60},
-                        cacheableResponse: {statuses: [0, 200]},
+module.exports = require('@zeit/next-less')(
+    require('next-images')(
+        require('next-offline')({
+            webpack(config) {
+                config.node = {fs: 'empty'};
+                return config;
+            },
+            env: {},
+            workboxOpts: {
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https?.*/,
+                        handler: 'networkFirst',
+                        options: {
+                            cacheName: 'https-calls',
+                            networkTimeoutSeconds: 15,
+                            expiration: {maxEntries: 150, maxAgeSeconds: 30 * 24 * 60 * 60},
+                            cacheableResponse: {statuses: [0, 200]},
+                        },
                     },
-                },
-            ],
-        },
-    })
+                ],
+            },
+        })
+    )
 );
